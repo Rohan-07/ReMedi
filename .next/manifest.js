b7a8ef5a1c390fa96656679f3da364edef4607eb
Shrutipancholi <shruti.p.822000@gmail.com>
2021-08-08 18:36:51 +0530

    updated index.js

--- .next/manifest.js
+++ .next/manifest.js
@@ -89,7 +89,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "f43d8f5e217ba1705045"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "d299f3c17731d00e210b"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -754,7 +754,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","2":"bundles\\pages\\_document.js","3":"commons","5":"bundles\\pages\\Patient\\upload.js","6":"bundles\\pages\\Patient\\index.js","7":"bundles\\pages\\Doctor\\index.js"}[chunkId]||chunkId) + "";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","2":"bundles\\pages\\_document.js","3":"commons","6":"bundles\\pages\\Patient\\index.js","8":"bundles\\pages\\Patient\\access.js"}[chunkId]||chunkId) + "";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
@@ -804,7 +804,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/_next/725d6b7e-ac3d-4de6-a798-757ec88bc2ca/webpack/";
+/******/ 	__webpack_require__.p = "/_next/c68505ae-6e8b-448a-a997-d54fd8b43fcd/webpack/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+//sdgfyjhgd,gkjmdhfbkj
 import {
 	Container,
 	Header,
